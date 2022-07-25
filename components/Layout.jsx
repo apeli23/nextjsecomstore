b7c99a3a0790a2af26a08b0680c54d326c71f11d
apeli23 <apelibrian@gmail.com>
2022-07-25 08:40:55 +0300

    header-links

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,27 +1,42 @@
 import React from "react";
 import Head from "next/head";
-
+import data from "../utils/data";
+import NextLink from "next/link";
 import {
     AppBar,
     Toolbar,
     Typography,
-    Container
+    Container,
+    Link
 } from "@mui/material";
 import useStyles from "../utils/styles";
 
 export default function Layout({ children }) {
-    const  classes = useStyles();
+    const classes = useStyles();
     return (
         <div>
             <Head>
                 <title>ecommerce</title>
             </Head>
             <AppBar position='static' className={classes.navbar}>
+
                 <Toolbar>
-                    <Typography>Nextjs ecommerce</Typography>
+                    <NextLink href="/" passHref>
+                        <Link>
+                            <Typography className={classes.brand}>Nextjs ecommerce</Typography></Link>
+                    </NextLink>
+                    <div className={classes.grow}></div>
+                    <div>
+                        <NextLink href="/cart" passHref>
+                            <Link>Cart</Link>
+                        </NextLink>
+                        <NextLink href="/login" passHref>
+                            <Link>Login</Link>
+                        </NextLink>
+                    </div>
                 </Toolbar>
             </AppBar>
-            <Container  className={classes.main}>{children}</Container>
+            <Container className={classes.main}>{children}</Container>
             <footer className={classes.footer}>
                 <Typography>All rights reserved. Nextjs Amazon</Typography>
             </footer>
